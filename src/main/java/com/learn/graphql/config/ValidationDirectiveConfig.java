3db69c7849f21da3f1cebdbfe79de6d5b5cc58c5
Philip Starritt <imphilsoftware@gmail.com>
2020-11-29 14:39:28 +0100

    refactoring configuration comments

--- src/main/java/com/learn/graphql/config/ValidationDirectiveConfig.java
+++ src/main/java/com/learn/graphql/config/ValidationDirectiveConfig.java
@@ -14,12 +14,12 @@ public class ValidationDirectiveConfig {
 
   @Bean
   public ValidationSchemaWiring validationSchemaWiring() {
-    // This contains by default the standard library provided @Directive constraints
+    // Contains the default validation @Directive constraints
     var validationRules = ValidationRules.newValidationRules()
         .onValidationErrorStrategy(OnValidationErrorStrategy.RETURN_NULL)
         .build();
 
-    // This will rewrite your data fetchers when rules apply to them so that validation
+    // Rewrites your data fetchers so that it's arguments are validated prior method execution
     return new ValidationSchemaWiring(validationRules);
   }
 
