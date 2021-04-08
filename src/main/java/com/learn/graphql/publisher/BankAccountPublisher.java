7d4b96957e8614d21a739a3cc11540f6c433f793
Philip Starritt <50707594+philip-jvm@users.noreply.github.com>
2021-04-08 15:04:56 +0200

    Chapter 33 (Subscriptions): Create Individual bank account subscriber  (#5)

--- src/main/java/com/learn/graphql/publisher/BankAccountPublisher.java
+++ src/main/java/com/learn/graphql/publisher/BankAccountPublisher.java
@@ -1,10 +1,11 @@
 package com.learn.graphql.publisher;
 
 import com.learn.graphql.domain.bank.BankAccount;
+import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.DirectProcessor;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxProcessor;
 import reactor.core.publisher.FluxSink;
 
@@ -15,6 +16,9 @@ import reactor.core.publisher.FluxSink;
 @Component
 public class BankAccountPublisher {
 
+  /**
+   * Use Sinks.Many with Reactor v3.4+
+   */
   private final FluxProcessor<BankAccount, BankAccount> processor;
   private final FluxSink<BankAccount> sink;
 
@@ -27,11 +31,20 @@ public class BankAccountPublisher {
     sink.next(bankAccount);
   }
 
-  public Flux<BankAccount> getBankAccountPublisher() {
+  public Publisher<BankAccount> getBankAccountPublisher() {
     return processor.map(bankAccount -> {
-      log.info("Publishing Bank Account {}", bankAccount);
+      log.info("Publishing global subscription update for Bank Account {}", bankAccount);
       return bankAccount;
     });
+  }
+
+  public Publisher<BankAccount> getBankAccountPublisherFor(UUID id) {
+    return processor
+        .filter(bankAccount -> id.equals(bankAccount.getId()))
+        .map(bankAccount -> {
+          log.info("Publishing individual subscription update for Bank Account {}", bankAccount);
+          return bankAccount;
+        });
   }
 
 }
