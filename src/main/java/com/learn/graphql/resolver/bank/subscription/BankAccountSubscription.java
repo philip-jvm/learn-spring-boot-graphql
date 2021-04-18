2fb18ca7bcc07a3d927c16c19abab756f34d6478
Philip Starritt <50707594+philip-jvm@users.noreply.github.com>
2021-04-18 12:23:34 +0200

    Chapter 34: Configure subscription Pre-Auth spring security (#7)

--- src/main/java/com/learn/graphql/resolver/bank/subscription/BankAccountSubscription.java
+++ src/main/java/com/learn/graphql/resolver/bank/subscription/BankAccountSubscription.java
@@ -1,28 +1,52 @@
 package com.learn.graphql.resolver.bank.subscription;
 
+import com.learn.graphql.config.security.AuthenticationConnectionListener;
 import com.learn.graphql.domain.bank.BankAccount;
 import com.learn.graphql.publisher.BankAccountPublisher;
+import graphql.kickstart.servlet.context.GraphQLWebSocketContext;
 import graphql.kickstart.tools.GraphQLSubscriptionResolver;
+import graphql.schema.DataFetchingEnvironment;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Publisher;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 /**
  * Subscription (Chapter 33)
  */
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class BankAccountSubscription implements GraphQLSubscriptionResolver {
 
   private final BankAccountPublisher bankAccountPublisher;
 
+  @PreAuthorize("hasAuthority('get:bank_account')")
   public Publisher<BankAccount> bankAccounts() {
     return bankAccountPublisher.getBankAccountPublisher();
   }
 
-  public Publisher<BankAccount> bankAccount(UUID id) {
+  @PreAuthorize("hasAuthority('get:bank_account')")
+  public Publisher<BankAccount> bankAccount(UUID id, DataFetchingEnvironment e) {
+    log.info("Creating bank account publisher for user Id: {}",
+        SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+
+    // As an alternative to spring-security, you can access the authentication via the DataFetchingEnvironment
+    GraphQLWebSocketContext context = getContext(e);
+    var authentication = (Authentication) context.getSession().getUserProperties()
+        .get(AuthenticationConnectionListener.AUTHENTICATION);
+    log.info("Creating bank account publisher for user Id: {}",
+        authentication.getPrincipal());
+
     return bankAccountPublisher.getBankAccountPublisherFor(id);
+  }
+
+  private <T> T getContext(DataFetchingEnvironment e) {
+    return e.getContext();
   }
 
 }
